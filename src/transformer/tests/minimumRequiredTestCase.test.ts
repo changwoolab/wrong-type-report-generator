6f8194727d38b83226ed36ce888cf772004123fd
changwoolab <cwyoo01@naver.com>
2023-05-28 17:59:07 +0900

    test: add array test

--- src/transformer/tests/minimumRequiredTestCase.test.ts
+++ src/transformer/tests/minimumRequiredTestCase.test.ts
@@ -38,11 +38,11 @@ describe('transformer - Minimum requirements', () => {
                         type: 'union',
                         argument: [
                             getNewAstNode({
-                                name: 'UnionElement',
+                                name: 'unionElement',
                                 type: 'null',
                             }),
                             getNewAstNode({
-                                name: 'UnionElement',
+                                name: 'unionElement',
                                 type: 'object',
                                 argument: [
                                     getNewAstNode({
@@ -75,15 +75,15 @@ describe('transformer - Minimum requirements', () => {
             type: 'union',
             argument: [
                 getNewAstNode({
-                    name: 'UnionElement',
+                    name: 'unionElement',
                     type: 'TEST2_ENUMS.test1',
                 }),
                 getNewAstNode({
-                    name: 'UnionElement',
+                    name: 'unionElement',
                     type: 'TEST2_ENUMS.test2',
                 }),
                 getNewAstNode({
-                    name: 'UnionElement',
+                    name: 'unionElement',
                     type: 'TEST2_ENUMS.test3',
                 }),
             ],
@@ -124,12 +124,62 @@ describe('transformer - Minimum requirements', () => {
         const newAst = transformer(typeDeclaration);
 
         // Then
+        const expected = getNewRootAst({
+            dependencies: new Map(),
+            astNode: getNewAstNode({
+                name: 'TEST4_ARRAY',
+                type: 'object',
+                argument: [
+                    getNewAstNode({
+                        name: 'test41',
+                        type: 'array',
+                        argument: [
+                            getNewAstNode({
+                                name: 'arrayElement',
+                                type: 'union',
+                                argument: [
+                                    getNewAstNode({
+                                        name: 'unionElement',
+                                        type: '"int1"',
+                                    }),
+                                    getNewAstNode({
+                                        name: 'unionElement',
+                                        type: '"int2"',
+                                    }),
+                                ],
+                            }),
+                        ],
+                    }),
+                    getNewAstNode({
+                        name: 'test42',
+                        type: 'array',
+                        argument: [
+                            getNewAstNode({
+                                name: 'arrayElement',
+                                type: 'union',
+                                argument: [
+                                    getNewAstNode({
+                                        name: 'unionElement',
+                                        type: '"int1"',
+                                    }),
+                                    getNewAstNode({
+                                        name: 'unionElement',
+                                        type: '"int2"',
+                                    }),
+                                ],
+                            }),
+                        ],
+                    }),
+                ],
+            }),
+        });
+        expect(newAst).toEqual(expected);
     });
 
-    test('TEST5_GENERICS', () => {
+    test('TEST4_1_ARRAY_WITH_OBJECT', () => {
         // Given
         const typeDeclaration = getTypeDeclaration(
-            'TEST5_GENERICS',
+            'TEST4_1_ARRAY_WITH_OBJECT',
             typeDeclarations,
         );
 
@@ -137,20 +187,86 @@ describe('transformer - Minimum requirements', () => {
         const newAst = transformer(typeDeclaration);
 
         // Then
+        const expected = getNewRootAst({
+            dependencies: new Map(),
+            astNode: getNewAstNode({
+                name: 'TEST4_1_ARRAY_WITH_OBJECT',
+                type: 'array',
+                argument: [
+                    getNewAstNode({
+                        name: 'arrayElement',
+                        type: 'object',
+                        argument: [
+                            getNewAstNode({
+                                name: 'int1',
+                                type: 'string',
+                            }),
+                            getNewAstNode({
+                                name: 'int2',
+                                type: 'union',
+                                argument: [
+                                    getNewAstNode({
+                                        name: 'unionElement',
+                                        type: 'null',
+                                    }),
+                                    getNewAstNode({
+                                        name: 'unionElement',
+                                        type: 'object',
+                                        argument: [
+                                            getNewAstNode({
+                                                name: 'int3',
+                                                type: 'string',
+                                            }),
+                                        ],
+                                    }),
+                                ],
+                            }),
+                        ],
+                    }),
+                ],
+            }),
+        });
+        expect(newAst).toEqual(expected);
     });
 
-    test('TEST6_APPLIED_GENERIC_OBJECTS', () => {
-        // Given
-        const typeDeclaration = getTypeDeclaration(
-            'TEST6_APPLIED_GENERIC_OBJECTS',
-            typeDeclarations,
-        );
+    // test('TEST5_GENERICS', () => {
+    //     // Given
+    //     const typeDeclaration = getTypeDeclaration(
+    //         'TEST5_GENERICS',
+    //         typeDeclarations,
+    //     );
 
-        // When
-        const newAst = transformer(typeDeclaration);
+    //     // When
+    //     const newAst = transformer(typeDeclaration);
 
-        // Then
-    });
+    //     // Then
+    // });
+
+    // test('TEST6_APPLIED_GENERIC_OBJECTS', () => {
+    //     // Given
+    //     const typeDeclaration = getTypeDeclaration(
+    //         'TEST6_APPLIED_GENERIC_OBJECTS',
+    //         typeDeclarations,
+    //     );
+
+    //     // When
+    //     const newAst = transformer(typeDeclaration);
+
+    //     // Then
+    // });
+
+    // test('TEST7_APPLIED_NAMESPACE', () => {
+    //     // Given
+    //     const typeDeclaration = getTypeDeclaration(
+    //         'TEST7_APPLIED_NAMESPACE',
+    //         typeDeclarations,
+    //     );
+
+    //     // When
+    //     const newAst = transformer(typeDeclaration);
+
+    //     // Then
+    // });
 
     // test('INTEGRATED_TEST', () => {
     //     // Given
