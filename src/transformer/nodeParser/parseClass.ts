6ae5126ffae0c6cff755c0d37a9a1abf302dbd4e
changwoolab <cwyoo01@naver.com>
2023-05-29 23:49:27 +0900

    fix: parse class as a interface
    Reason: when using "instanceof" with axios response, it cannot check error correctly

--- src/transformer/nodeParser/parseClass.ts
+++ src/transformer/nodeParser/parseClass.ts
@@ -1,13 +1,64 @@
 import { getNewAstNode } from '../../reporterAst/astUtils';
 import { addTypeToDependencyMap } from '../../utils';
-import { ParseNode } from './parseNode';
+import { ParseNode, parseNode } from './parseNode';
+import { Node } from 'ts-morph';
 
 export const parseClass = ({ name, type, addToDependencyMap }: ParseNode) => {
     // add to dependency then use "instanceof" operator when generating code
     addTypeToDependencyMap(type, addToDependencyMap);
 
-    return getNewAstNode({
+    // But when using "instanceof" operator
+    // it may not be possible to check the type from axios response
+    // So, parse as a interface
+    const astNode = getNewAstNode({
         name,
         type: 'class',
+        argument: [],
     });
+
+    const symbol = type.getSymbol();
+    if (!symbol) {
+        throw new Error('No class Symbols');
+    }
+
+    // class declaration is only one
+    const declaration = symbol.getDeclarations()[0];
+
+    if (!Node.isClassDeclaration(declaration)) {
+        throw new TypeError('Extected declaration to be an class declaration');
+    }
+
+    // parse base
+    const parsedBases = declaration.getBaseTypes().map((baseType) => {
+        return parseNode({
+            name: type.getSymbol()?.getName() as string,
+            type: baseType,
+            addToDependencyMap,
+        });
+    });
+    parsedBases.forEach((node) => {
+        astNode.arguments?.push(node);
+    });
+
+    // parse properties
+    const properties = [
+        ...declaration.getProperties(),
+        ...declaration.getMethods(),
+    ].map((p) => ({
+        name: p.getSymbol()?.getEscapedName() ?? p.getName(),
+        type: p.getType(),
+    }));
+
+    const parsedProperties = properties.map((prop) => {
+        return parseNode({
+            name: prop.name,
+            type: prop.type,
+            addToDependencyMap,
+        });
+    });
+    parsedProperties.forEach((node) => {
+        astNode.arguments?.push(node);
+    });
+
+    return astNode;
 };
