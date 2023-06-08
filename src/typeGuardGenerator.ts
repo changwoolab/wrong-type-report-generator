2f22827922d21f182a2c7cc811428cea8c407ded
changwoolab <cwyoo01@naver.com>
2023-06-08 21:08:26 +0900

    feat: report generator

--- src/typeGuardGenerator.ts
+++ src/typeGuardGenerator.ts
@@ -1,20 +1,24 @@
 import { Project, SourceFile } from 'ts-morph';
 import { transformer } from './transformer/transformer';
 import { isTypeDeclaration, TypeDeclaration } from './types';
+import { codeGenerator } from './codeGenerator/codeGenerator';
+import path from 'path';
 
-const getGuardName = (typeDeclaration: TypeDeclaration) => {
-    return 'is' + typeDeclaration.getName();
-};
-
-export const generateTypeGuard = ({
+export const generateOneTypeGuard = async ({
     filePath,
     project,
-}: // savePath,
-{
+    outDirPath,
+    onlyExport,
+}: {
     filePath: string;
     project: Project;
-    // savePath: string;
+    outDirPath?: string;
+    onlyExport?: boolean;
 }) => {
+    if (!outDirPath) {
+        outDirPath = path.dirname(filePath);
+    }
+
     const sourceFile = project.getSourceFile(filePath);
     if (!sourceFile) {
         throw new Error(`No sourcefile at ${filePath}`);
@@ -24,38 +28,56 @@ export const generateTypeGuard = ({
         .flat()
         .filter((ex) => ex.getSourceFile() === sourceFile);
 
-    const typeDeclarations = exports.filter(isTypeDeclaration);
+    const exportedTypeDeclarations = exports.filter(isTypeDeclaration);
 
     // Get all type declarations
-    const allTypesDeclarations: TypeDeclaration[] = [
-        ...sourceFile.getInterfaces(),
-        ...sourceFile.getTypeAliases(),
-        ...sourceFile.getEnums(),
-    ];
+    const typesDeclarations: TypeDeclaration[] = onlyExport
+        ? exportedTypeDeclarations
+        : [
+              ...sourceFile.getInterfaces(),
+              ...sourceFile.getTypeAliases(),
+              ...sourceFile.getEnums(),
+          ];
 
     // Get new AST
-    const newAsts = allTypesDeclarations.map((typeDeclaration) => {
+    const newAsts = typesDeclarations.map((typeDeclaration) => {
         return transformer(typeDeclaration);
     });
 
     // Generate guard then add to SourceFile
-    // allTypesDeclarations.forEach((typeDeclaration) => {
-    //     const guardName = getGuardName(typeDeclaration);
-    // });
-
-    // save guard file
+    await Promise.all(
+        newAsts.map((ast) => {
+            return codeGenerator({
+                astRootNode: ast,
+                project,
+                outFilePath: `${outDirPath}/${ast.ast.name}.guard.ts`,
+                inputSourceFile: sourceFile,
+            });
+        }),
+    );
 };
 
-generateTypeGuard({ filePath: 'test/interface.ts', project: new Project() });
+export const generateTypeGuard = async ({
+    filePaths,
+    project,
+    outDirPath,
+    onlyExport,
+}: {
+    filePaths: string[];
+    project: Project;
+    outDirPath?: string;
+    onlyExport?: boolean;
+}) => {
+    await Promise.all(
+        filePaths.map((filePath) => {
+            return generateOneTypeGuard({
+                filePath,
+                project,
+                outDirPath,
+                onlyExport,
+            });
+        }),
+    );
+};
 
-const _generateTypeGuard = () => {};
-
-// 타입 가드 만들기
-// primitive 값 만남 -> 그냥 바로 타입 리포트에 담아서 리턴
-// Non-primitive 만났을 때
-//      일단 기본적으로 재귀적으로 한다. 타입 체크를 해서 타입 정의마다 할 행동을 정의한 함수로 보내주는 함수를 만들기.
-//      Primitive level까지 내려가지 못하는 것들 (Array 등) -> Non-primitive를 처리해주는 함수로 보내고 리턴받아서
-//      그 값 + 현재 컬럼 이름을 리포트에 넣기
-//      Array Type인지 판별해주는 함수를 만드는 함수로 보내줘야함.
-
-// Array 처리
+generateOneTypeGuard({ filePath: 'test/interface.ts', project: new Project() });
